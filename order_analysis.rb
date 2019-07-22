996c7bdf5476c057a0381c63e27ee693560d314b
Thomas Ritter <thomas.ritter@mocoapp.com>
2019-07-22 20:22:28 +0200

    Initial version

---
+++ order_analysis.rb
@@ -0,0 +1,33 @@
+class OrderAnalysis
+  CSV_DELIMITER = ";"
+
+  def analyze
+    analyze_customers
+  end
+
+  def analyze_customers
+    customers_csv = File.read("customers.csv")
+    customer_lines = customers_csv.split("\n")
+
+    customer_lines.each do |customer_line|
+      extract_customer(customer_line)
+    end
+  end
+
+  def extract_customer(line)
+    parts = line.split(CSV_DELIMITER)
+    if parts.size != 4
+      puts "[ERROR] Something's wrong with this customer line: #{line}!"
+      return
+    end
+
+    id = parts[0]
+    first_name = parts[1]
+    last_name = parts[2]
+    location = parts[3]
+
+    puts "Customer #{id}: #{first_name} #{last_name}"
+  end
+end
+
+OrderAnalysis.new.analyze
